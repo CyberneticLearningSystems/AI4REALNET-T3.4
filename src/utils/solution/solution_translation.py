e14857f01b1f6203e6d24b3cda4e101ffb130e40
ozmanda <usher.juliapatricia@gmail.com>
2025-11-24 10:02:23 +0100

    initial commit solution translation

--- src/utils/solution/solution_translation.py
+++ src/utils/solution/solution_translation.py
@@ -1,14 +1,26 @@
+from typing import List, Tuple
 from flatland.envs.rail_env import RailEnv
+from src.utils.graph.flatland_railway_extension.RailroadSwitchAnalyser import RailroadSwitchAnalyser
+from src.utils.graph.flatland_railway_extension.RailroadSwitchCluster import RailroadSwitchCluster
+from src.utils.graph.MultiDiGraphBuilder import MultiDiGraphBuilder
 
-def SolutionTranslator(): 
+class SolutionTranslator(): 
     def __init__(self, env: RailEnv):
-        self.env = env
+        self.multidigraph = MultiDiGraphBuilder(env)
 
-    def encode_solution(self, coordinate_path):
+    def encode_solution(self, coordinate_path) -> List[int]:
+        """
+        Translates a list of coordinates into a solution path for the Flatland environment.
+        """
         # TODO: implement translation coordinates -> path
-        raise NotImplementedError("Solution generation not implemented yet.")
+        # raise NotImplementedError("Solution generation not implemented yet.")
+        pass
     
-    def decode_solution(self, track_path):
+    def decode_solution(self, track_path) -> List[Tuple[int, int]]:
+        """
+        Translates a solution path from the Flatland environment into a list of coordinates that the agent can follow. 
+        """
         # TODO: implement translation path -> coordinates
-        raise NotImplementedError("Solution decoding not implemented yet.")    
+        # raise NotImplementedError("Solution decoding not implemented yet.")    
+        pass
     
