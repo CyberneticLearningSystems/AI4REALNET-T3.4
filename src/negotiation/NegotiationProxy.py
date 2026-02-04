75191fea38820cbed945f326ff9ed53a25e616b1
ozmanda <usher.juliapatricia@gmail.com>
2026-02-04 13:38:10 +0100

    negotiation proxy update + readme

--- src/negotiation/NegotiationProxy.py
+++ src/negotiation/NegotiationProxy.py
@@ -5,16 +5,35 @@ class NegotiationProxy:
     def __init__(self):
         self.params: Dict = {}
 
-    def init_params(self, negotiation_params: Dict):
+    def _init_params(self, negotiation_params: Dict):
         self.params = negotiation_params
 
-    def adjust_param(self, new_params: Dict):
+    def add_params(self, new_params: Dict):
+        """
+        Add new negotiation parameters during runtime based on human input.
+        """
+        for key, value in new_params.items():
+            if key not in self.params:
+                self.params[key] = value    
+
+    def adjust_param(self, new_param: str, new_value):
+        """
+        Adjust a single negotiation parameter during runtime based on human input.
+        """
+        if new_param in self.params:
+            self.params[new_param] = new_value
+        else: 
+            Warning(f"Parameter {new_param} not found in negotiation parameters.")
+
+    def adjust_params(self, new_params: Dict):
         """
         Adjust of negotiation parameters during runtime based on human input.
         """
         for key, value in new_params.items():
             if key in self.params:
                 self.params[key] = value
+            else:
+                Warning(f"Parameter {key} not found in negotiation parameters.")
 
     def perform_negotiation(self):
         """
---
+++ src/negotiation/README.md
@@ -0,0 +1,6 @@
+# Negotiation Proxy
+This folder contains all files pertaining to the automated negotiation between agents in the automated system. 
+
+:construction: Work in Progress :construction:
+
+The current outline for the negotiation proxy consists of a series of parameters which define the preferred weighting of objectives by the human operator. The negotiation is planned to be carried out using a bidding method, wherein agents put forward the expected reward of their local solution. In combination with the solution parameters set by the human, the solutions are compared, and the prevailing solution chosen. 
